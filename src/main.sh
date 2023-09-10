31370afcd8f03c61ebee5e5d22b328daf88acae5
Hein Tonny Køien <hein.koien@blinq.no>
2023-09-10 13:59:23 +0000

    Update test github action workflow

--- src/main.sh
+++ src/main.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Function to remove ANSI escape codes
+function remove_ansi_codes() {
+  echo "$1" | sed 's/\x1b\[[0-9;]*m//g'
+}
+
 # Function to extract essential output from Terraform plan
 function extract_essential_output() {
   cat "$1" | sed -n '/Terraform used the selected providers/,/Terraform plan return code:/p'
@@ -24,12 +29,15 @@ fi
 
 # Capture the Terraform plan output into a variable
 plan_output=$(cat "$output_file")
+
+# Extract the essential part of the output and remove ANSI codes
 essential_output=$(extract_essential_output "$output_file")
+clean_essential_output=$(remove_ansi_codes "$essential_output")
 
 # Comment on PR
-if [[ -z $essential_output ]]; then
+if [[ -z $clean_essential_output ]]; then
   comment_on_pr "Terraform essential output is empty. Please check the plan output."
 else
-  comment_body=$(printf "## Terraform Plan Essential Output\n\`\`\`\n%s\n\`\`\`\n" "$essential_output")
+  comment_body=$(printf "## Terraform Plan Essential Output\n\`\`\`\n%s\n\`\`\`\n" "$clean_essential_output")
   comment_on_pr "$comment_body"
 fi
