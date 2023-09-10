254ad0c187d46f51f4a095231ff1e75381299ce3
Hein Tonny Køien <hein.koien@blinq.no>
2023-09-10 13:43:21 +0000

    Update test github action workflow

--- terraform/vnet.tf
+++ terraform/vnet.tf
@@ -5,15 +5,19 @@ resource "azurerm_virtual_network" "example" {
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "examle" {
+resource "azurerm_subnet" "example" {
   name                                          = "internal"
   resource_group_name                           = azurerm_resource_group.example.name
   virtual_network_name                          = azurerm_virtual_network.example.name
-  address_prefixes                              = "10.0.1.0/24"
+  address_prefixes                              = ["10.0.1.0/24"]
   service_endpoints                             = ["Microsoft.Storage"]
-  network_security_group_id                     = azurerm_network_security_group.example.id
   private_endpoint_network_policies_enabled     = true
   private_link_service_network_policies_enabled = true
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
 }
 
 resource "azurerm_subnet_route_table_association" "example" {
